7eeed23cbf5a707e93c6ea3a5c298609f8aa76c2
Golden Eagle <joowoon.kang@biola.edu>
2020-05-03 22:44:55 -0700

    console.logging props on Homepage for education purposes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/topics" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
