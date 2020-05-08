f2daeeb95f62754dc83ca5a97b71aece8fe86c4a
Golden Eagle <joowoon.kang@biola.edu>
2020-05-07 22:56:52 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = (props) => (
   <div>
@@ -16,7 +17,7 @@ class App extends Component {
       <div className="App">
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/topics" component={HatsPage} />
+          <Route path="/shop" component={ShopPage} />
         </Switch>
       </div>
     );
