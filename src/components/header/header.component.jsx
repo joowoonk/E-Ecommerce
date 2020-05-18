203782c83d7117b68e56c083adbfbc0c4c896d18
Golden Eagle <joowoon.kang@biola.edu>
2020-05-17 21:45:20 -0700

    custom button and header being replaced by styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,7 +16,7 @@ import {
   LogoContainer,
   OptionsContainer,
   OptionLink,
-  OptionDiv,
+  // OptionDiv,
 } from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => {
@@ -29,7 +29,9 @@ const Header = ({ currentUser, hidden }) => {
         <OptionLink to="/shop">SHOP</OptionLink>
         <OptionLink to="/contact">CONTACT</OptionLink>
         {currentUser ? (
-          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+          <OptionLink as="div" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </OptionLink>
         ) : (
           <OptionLink to="/signin">SIGN IN</OptionLink>
         )}
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,11 +1,11 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import { Link } from "react-router-dom";
 
-const OptionContainerStyles = css`
-  padding: 10px 15px;
-  cursor: pointer;
-`;
+// const OptionContainerStyles = css`
+//   padding: 10px 15px;
+//   cursor: pointer;
+// `; If you want to use css you want to import from styled, {css} like this
 
 export const HeaderContainer = styled.div`
   height: 70px;
@@ -30,9 +30,10 @@ export const OptionsContainer = styled.div`
 `;
 
 export const OptionLink = styled(Link)`
-  ${OptionContainerStyles}
+  padding: 10px 15px;
+  cursor: pointer;
 `;
 
-export const OptionDiv = styled.div`
-  ${OptionContainerStyles}
-`;
+// export const OptionDiv = styled.div`
+//   ${OptionContainerStyles}
+// `;
